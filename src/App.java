21ae7c273d64b1c378ab00329b7b31f9d984fd5c
PedroP57xd <pedropan2906@hotmail.com>
2025-10-28 08:11:11 -0500

    first

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] numeros ={5,0,-1,4,9,6,-5,2,3};
+        
+        SortShell sortShell= new SortShell();
+        View view = new View();
+
+        view.printArray(numeros);
+        sortShell.sort(numeros, false);
+        view.printArray(numeros);
     }
 }
--- src/SortShell.java
+++ src/SortShell.java
@@ -1,10 +1,23 @@
 public class SortShell {
-    
-    public SortShell(){
+
+    public SortShell() {
         System.out.println("Metodo Shell");
     }
 
-    public void sort(int[] numeros, boolean asce){
-
+    public void sort(int[] numeros, boolean asce) {
+        int n = numeros.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            int cambios = 0;
+            for (int i = gap; i < n; i++) {
+                int temp = numeros[i];
+                int j = i;
+                while (j >= gap && numeros[j - gap] > temp) {
+                    j -= gap;
+                    cambios++;
+                    numeros[j + 1] = numeros[j];
+                }
+                numeros[j] = temp;
+            }
+        }
     }
 }
