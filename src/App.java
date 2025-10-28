ed3e8773002631a0e5e069b1c2360d3fd4e387f7
PedroP57xd <pedropan2906@hotmail.com>
2025-10-28 08:42:01 -0500

    comparaciones

--- src/App.java
+++ src/App.java
@@ -1,12 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] numeros ={5,0,-1,4,9,6,-5,2,3};
-        
-        SortShell sortShell= new SortShell();
+        int[] numeros = { 5, 0, 8, 4, 9, 6, 7, 2, 3 };
+
+        SortShell sortShell = new SortShell();
         View view = new View();
 
+        System.out.println("\n Arreglo Original");
         view.printArray(numeros);
+        System.out.println("\n Adrreglo Ordenado");
         sortShell.sort(numeros, false);
         view.printArray(numeros);
+        System.out.println("\n ");
     }
 }
--- src/SortShell.java
+++ src/SortShell.java
@@ -6,18 +6,21 @@ public class SortShell {
 
     public void sort(int[] numeros, boolean asce) {
         int n = numeros.length;
+        int comparacaciones=0;
         for (int gap = n / 2; gap > 0; gap /= 2) {
             int cambios = 0;
             for (int i = gap; i < n; i++) {
                 int temp = numeros[i];
                 int j = i;
+                comparacaciones++;
                 while (j >= gap && numeros[j - gap] > temp) {
+                    numeros[j] = numeros[j-gap];
                     j -= gap;
                     cambios++;
-                    numeros[j + 1] = numeros[j];
                 }
                 numeros[j] = temp;
             }
         }
+        System.out.println(comparacaciones);
     }
 }
